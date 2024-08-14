ddca6f3441fe1c9e522bc40f62517e51edbdf4a0
alterdekim <alterwain@protonmail.com>
2024-08-14 14:12:15 +0300

    Changes to be committed: 	modified:   src/tcp_client.rs 	modified:   src/tcp_server.rs

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -18,6 +18,7 @@ pub async fn server_mode(bind_addr: String) {
     
     let mut config = tun2::Configuration::default();
     config.address("10.8.0.1");
+    config.netmask("255.255.255.0");
     config.tun_name("tun0");
     config.up();
 
@@ -114,7 +115,7 @@ pub async fn server_mode(bind_addr: String) {
             loop {
                 if let Ok(bytes) = thread_mx.recv() {
                     let vpn_packet = VpnPacket::init(bytes);
-                    let serialized_data = bincode::serialize::<VpnPacket>(&vpn_packet).unwrap();
+                    let serialized_data = bincode::serialize(&vpn_packet).unwrap();
                     sock_writer.write_all(&serialized_data).await.unwrap();
                     //info!("Wrote to sock: {:?}", serialized_data);
                 }
