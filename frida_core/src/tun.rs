e922ee30337711a8c60d405ec2b47213549cde0f
alterdekim <alterwain@protonmail.com>
2024-12-09 02:10:33 +0300

    modified:   frida_core/src/tun.rs

--- frida_core/src/tun.rs
+++ frida_core/src/tun.rs
@@ -5,6 +5,5 @@ use crate::win_tun::{DeviceReader, DeviceWriter, create};
 use crate::linux_tun::{DeviceReader, DeviceWriter, create};
 
 pub(crate) fn create_tun() -> (DeviceReader, DeviceWriter) {
-    #[cfg(target_os = "windows")]
     create()
 }
