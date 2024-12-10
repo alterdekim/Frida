7303c2837045ae1fc256b0eacf64ee05157fe268
alterdekim <alterwain@protonmail.com>
2024-12-11 01:49:00 +0300

    modified:   Cargo.lock 	modified:   frida_core/Cargo.toml 	modified:   frida_core/src/android_tun.rs

--- frida_core/src/android_tun.rs
+++ frida_core/src/android_tun.rs
@@ -1,15 +1,27 @@
-use tokio::fs::File;
+//use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std::error::Error;
 use std::os::fd::FromRawFd;
 
+use std::fs::File;
+
+use libc::fdopen;
+use std::ffi::CString;
+
 pub fn create(cfg: i32) -> (DeviceReader, DeviceWriter) {
     // check this if android build won't work
+    /*let mode_read = CString::new("r").unwrap();
+    let mode_write = CString::new("w").unwrap();
+    let fd1 = cfg.clone();
+    let fd2 = fd1.clone();
+    let reader = unsafe { fdopen(fd1, mode_read.as_ptr()) };
+    let writer = unsafe { fdopen(fd2, mode_write.as_ptr()) };*/
+
     let fd1 = cfg.clone();
     let fd2 = fd1.clone();
     let mut reader = unsafe { File::from_raw_fd(fd1) };
     let mut writer = unsafe { File::from_raw_fd(fd2) };
-    
+
     (DeviceReader {reader}, DeviceWriter {writer})
 }
 
@@ -23,12 +35,12 @@ pub struct DeviceReader {
 
 impl DeviceWriter {
     pub async fn write(&mut self, buf: &Vec<u8>) -> Result<usize, Box<dyn Error>> {
-        Ok(self.writer.write(buf).await?)
+        Ok(self.writer.write(buf)?)
     }
 }
 
 impl DeviceReader {
     pub async fn read(&mut self, buf: &mut Vec<u8>) -> Result<usize, Box<dyn Error>> {
-        Ok(self.reader.read(buf).await?)
+        Ok(self.reader.read(buf)?)
     }
 }
