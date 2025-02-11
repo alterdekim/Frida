9474cb8652ea363dc7be88d6f4250223168ad457
alterwain@protonmail.com <alterwain@protonmail.com>
2025-02-11 05:16:43 +0300

    modified:   frida_core/build.rs

--- frida_core/build.rs
+++ frida_core/build.rs
@@ -2,7 +2,6 @@
 use cc;
 
 fn main() {
-    if cfg!(target_os = "macos") {
-        cc::Build::new().file("src/c/utun.c").compile("utun");
-    }
+    #[cfg(target_os = "macos")]
+    cc::Build::new().file("src/c/utun.c").compile("utun");
 }
